02223caa10062e1c9d02dfb06ac5548121792cc9
Purusoth Mahendran <purusoth1091@gmail.com>
2024-10-05 17:08:36 -0700

    Update next.config.js to allow Wikipedia image domains

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['upload.wikimedia.org'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/wikipedia/commons/**',
+      },
+    ],
+  },
+};
+module.exports = nextConfig
